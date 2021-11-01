13c84381315ba6041b6b7d7f2fdafc609d78be78
Lucas Ferreira <lcklucassocial@gmail.com>
2021-11-01 11:11:47 -0300

    commit

---
+++ assets/js/logic.js
@@ -0,0 +1,13 @@
+class Despesa { // Criação de uma classe para referenciar o objeto "despesas"
+
+    constructor(ano, mes, dia, tipo, descricao, valor) { // constructor recebendo parâmetros que serão recuperados na criação de um novo objeto...
+
+        // atributos do objeto "despesas"
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
